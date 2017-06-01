f7bfdf348455640bedb949c8d52db97e454fa357
mizanurriyadh <mizanurriyadh@ymail.com>
2017-06-02 02:59:09 +0600

    Map size

---
+++ map_size.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    map <char, int> mymap;
+
+    mymap['a'] = 101;
+    mymap['b'] = 102;
+    mymap['c'] = 103;
+
+    cout<<"Size: "<<mymap.size()<<endl;
+}
